4956f1ca37fef89eaa3948d2a9200ed31e0a2b0e
SAMIUR RAHMAN <S72RAHMA@uwaterloo.ca>
2020-12-30 16:41:42 -0500

    nearly completed scraping of site data, need to retrieve month data for dynamic scraping

---
+++ scrape.py
@@ -0,0 +1,40 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from pathlib import Path
+
+month_range = ['october', 'november', 'december', 'january', 'february', 'march', 'april', 'may', 'june']
+
+for year in range(1981, 2019):
+    for month in month_range:
+        url = 'https://www.basketball-reference.com/leagues/NBA_{}_games-{}.html'.format(year, month)
+        page = requests.get(url)
+
+        soup = BeautifulSoup(page.content, 'html.parser')
+        table = soup.find(id='schedule')
+        body = table.find('tbody')
+
+        df = pd.DataFrame(columns=['Date', 'Visitor/Neutral', 'PTS_1', 'Home/Neutral', 'PTS_2', '_1', '_2', 'Attend.', 'Notes'])
+
+        for index, row in enumerate(body.find_all('tr')):
+            row_data = []
+            for item in row:
+                row_data.append(item.get_text())
+            if len(row_data) == 9:
+                df_row = {
+                    'Date': row_data[0],
+                    'Visitor/Neutral': row_data[1],
+                    'PTS_1': row_data[2],
+                    'Home/Neutral': row_data[3],
+                    'PTS_2': row_data[4],
+                    '_1': row_data[5],
+                    '_2': row_data[6],
+                    'Attend.': row_data[7],
+                    'Notes': row_data[8],
+                }
+                df.loc[index] = df_row
+
+        output_file = '{}.csv'.format(month)
+        output_directory = Path('/Users/samiurrahman98/Desktop/Programming/nba-attendance-trends/output/{}/'.format(year))
+        output_directory.mkdir(parents=True, exist_ok=True)
+        df.to_csv(path_or_buf=(output_directory / output_file), index=False)
